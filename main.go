5e5dff668616166f22516b8f6e16e91fd8b29119
chenjiahao <chen-jh@outlook.com>
2020-12-16 16:13:34 +0800

    add timeout

--- main.go
+++ main.go
@@ -1,103 +1,118 @@
 package main
 
 import (
-    "flag"
-    assetfs "github.com/elazarl/go-bindata-assetfs"
-    "github.com/gin-gonic/gin"
-    "html/template"
-    "log"
-    "net/http"
-    "strconv"
-    "strings"
-    "video/asset"
-    v1 "video/routers/v1"
+	"flag"
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"video/asset"
+	v1 "video/routers/v1"
 )
 
 var port = flag.Int("port", 3030, "端口号")
 
 func main() {
-    flag.Parse()
-    gin.SetMode(gin.ReleaseMode)
-    r := gin.Default()
+	flag.Parse()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
 
-    //加载模板文件
-    t, err := loadTemplate()
-    if err != nil {
-        panic(err)
-    }
+	//加载模板文件
+	t, err := loadTemplate()
+	if err != nil {
+		panic(err)
+	}
 
-    r.SetHTMLTemplate(t)
+	r.SetHTMLTemplate(t)
 
-    //加载静态资源，例如网页的css、js
-    fs := assetfs.AssetFS{
-        Asset:     asset.Asset,
-        AssetDir:  asset.AssetDir,
-        AssetInfo: nil,
-        Prefix:    "static", //一定要加前缀
-    }
-    r.StaticFS("/static", &fs)
+	//加载静态资源，例如网页的css、js
+	fs := assetfs.AssetFS{
+		Asset:     asset.Asset,
+		AssetDir:  asset.AssetDir,
+		AssetInfo: nil,
+		Prefix:    "static", //一定要加前缀
+	}
+	r.StaticFS("/static", &fs)
 
-    //r.LoadHTMLGlob("template/*")
-    //r.Static("/static", "static")
+	//r.LoadHTMLGlob("template/*")
+	//r.Static("/static", "static")
 
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-    v1group := r.Group("/v1")
-    {
-        v1group.GET("/live", v1.Live)
-        v1group.GET("/hls", v1.Hls)
-        v1group.GET("/dash", v1.Dash)
-    }
+	v1group := r.Group("/v1")
+	{
+		v1group.GET("/live", v1.Live)
+		v1group.GET("/hls", v1.Hls)
+		v1group.GET("/dash", v1.Dash)
+	}
 
-    r.GET("/live", func(c *gin.Context) {
-        url := c.Query("url")
-        c.HTML(http.StatusOK, "flv.html", gin.H{
-            "url": url,
-        })
-    })
+	r.GET("/live", func(c *gin.Context) {
+		url := c.Query("url")
+		timeout := c.Query("timeout")
+		c.HTML(http.StatusOK, "flv.html", gin.H{
+			"url":     url,
+			"timeout": timeout,
+		})
+	})
 
-    r.GET("/hls", func(c *gin.Context) {
-        url := c.Query("url")
-        c.HTML(http.StatusOK, "hls.html", gin.H{
-            "url": url,
-        })
-    })
+	r.GET("/hls", func(c *gin.Context) {
+		url := c.Query("url")
+		timeout := c.Query("timeout")
+		c.HTML(http.StatusOK, "hls.html", gin.H{
+			"url":     url,
+			"timeout": timeout,
+		})
+	})
 
-    r.GET("/dash", func(c *gin.Context) {
-        url := c.Query("url")
-        c.HTML(http.StatusOK, "dash.html", gin.H{
-            "url": url,
-        })
-    })
+	r.GET("/dash", func(c *gin.Context) {
+		url := c.Query("url")
+		timeout := c.Query("timeout")
+		c.HTML(http.StatusOK, "dash.html", gin.H{
+			"url":     url,
+			"timeout": timeout,
+		})
+	})
 
-    log.Println("init...")
-    log.Print("sever listen port "+ strconv.Itoa(*port))
-    errRun := r.Run(":" + strconv.Itoa(*port))
-    if errRun != nil {
-        log.Fatal(errRun)
-    }
+	// 打印横幅
+	file, err := ioutil.ReadFile("banner.txt")
+	if err != nil {
+		log.Println(err.Error())
+	} else {
+		log.Println("\n" + string(file))
+	}
+
+	log.Println("init...")
+	log.Println("sever listen port " + strconv.Itoa(*port))
+	errRun := r.Run(":" + strconv.Itoa(*port))
+	if errRun != nil {
+		log.Fatal(errRun)
+	}
 }
 
 //加载模板文件
 func loadTemplate() (*template.Template, error) {
-    t := template.New("")
-    for _, name := range asset.AssetNames() {
-        if !strings.HasSuffix(name, ".html") {
-            continue
-        }
-        asset, err := asset.Asset(name)
-        if err != nil {
-            continue
-        }
-        name := strings.Replace(name, "template/", "", 1) //这里将templates替换下，在控制器中调用就不用加templates/了
-        t, err = t.New(name).Parse(string(asset))
-        if err != nil {
-            return nil, err
-        }
-    }
-    return t, nil
+	t := template.New("")
+	for _, name := range asset.AssetNames() {
+		if !strings.HasSuffix(name, ".html") {
+			continue
+		}
+		asset, err := asset.Asset(name)
+		if err != nil {
+			continue
+		}
+		name := strings.Replace(name, "template/", "", 1) //这里将templates替换下，在控制器中调用就不用加templates/了
+		t, err = t.New(name).Parse(string(asset))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
 }
--- routers/v1/page.go
+++ routers/v1/page.go
@@ -1,66 +1,70 @@
 package v1
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
-    "strings"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
-func Live(c *gin.Context)  {
-    ip, port, key, done := parsePath(c)
-    if done {
-        return
-    }
+func Live(c *gin.Context) {
+	ip, port, key, timeout, done := parsePath(c)
+	if done {
+		return
+	}
 
-    var url = "http://" + ip + ":" + port + "/live?app=live&stream=" + key
+	var url = "http://" + ip + ":" + port + "/live?app=live&stream=" + key
 
-    c.HTML(http.StatusOK, "flv.html", gin.H{
-        "url": url,
-    })
+	c.HTML(http.StatusOK, "flv.html", gin.H{
+		"url":     url,
+		"timeout": timeout,
+	})
 }
 
-func Hls(c *gin.Context)  {
-    ip, port, key, done := parsePath(c)
-    if done {
-        return
-    }
+func Hls(c *gin.Context) {
+	ip, port, key, timeout, done := parsePath(c)
+	if done {
+		return
+	}
 
-    var url = "http://" + ip + ":" + port + "/hls/" + key + ".m3u8"
+	var url = "http://" + ip + ":" + port + "/hls/" + key + ".m3u8"
 
-    c.HTML(http.StatusOK, "hls.html", gin.H{
-        "url": url,
-    })
+	c.HTML(http.StatusOK, "hls.html", gin.H{
+		"url":     url,
+		"timeout": timeout,
+	})
 }
 
-func Dash(c *gin.Context)  {
-    ip, port, key, done := parsePath(c)
-    if done {
-        return
-    }
+func Dash(c *gin.Context) {
+	ip, port, key, timeout, done := parsePath(c)
+	if done {
+		return
+	}
 
-    var url = "http://" + ip + ":" + port + "/dash/" + key + "/index.mpd"
+	var url = "http://" + ip + ":" + port + "/dash/" + key + "/index.mpd"
 
-    c.HTML(http.StatusOK, "dash.html", gin.H{
-        "url": url,
-    })
+	c.HTML(http.StatusOK, "dash.html", gin.H{
+		"url":     url,
+		"timeout": timeout,
+	})
 }
 
 // 解析路径参数
-func parsePath(c *gin.Context) (string, string, string, bool) {
-    ip := c.Query("ip")
-    if ip == "" {
-        ip = strings.Split(c.Request.Host, ":")[0]
-    }
-    port := c.Query("port")
-    if port == "" {
-        port = "80"
-    }
-    key := c.Query("key")
-    if key == "" {
-        c.JSON(http.StatusOK, gin.H{
-            "msg": "请求参数缺少key",
-        })
-        return "", "", "", true
-    }
-    return ip, port, key, false
+func parsePath(c *gin.Context) (string, string, string, string, bool) {
+	ip := c.Query("ip")
+	if ip == "" {
+		ip = strings.Split(c.Request.Host, ":")[0]
+	}
+	port := c.Query("port")
+	if port == "" {
+		port = "80"
+	}
+	key := c.Query("key")
+	if key == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "请求参数缺少key",
+		})
+		return "", "", "", "", true
+	}
+	timeout := c.Query("timeout")
+	return ip, port, key, timeout, false
 }
